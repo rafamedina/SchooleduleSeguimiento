b4cb74eed37307332db24117acbdd92e41c7aa4d
Rafael Medina <rafael.medina.ayuso@gmail.com>
2026-04-21 20:33:36 +0200

    Refactor para entrega del 50%

--- src/test/java/com/tfg/schooledule/SchooleduleApplicationTests.java
+++ src/test/java/com/tfg/schooledule/SchooleduleApplicationTests.java
@@ -2,10 +2,8 @@ package com.tfg.schooledule;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.test.context.ActiveProfiles;
 
 @SpringBootTest
-@ActiveProfiles("test")
 class SchooleduleApplicationTests {
 
   @Test
